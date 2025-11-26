8689a6fbe18a410875461aadc23021cfcf557fa3
mduy2k5 <maiduy2k5@gmail.com>
2025-11-26 20:49:56 +0700

    Update top-stores.blade.php to include custom stylesheet and fix stylesheet order

--- resources/views/top-stores.blade.php
+++ resources/views/top-stores.blade.php
@@ -9,8 +9,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/litepicker/dist/css/litepicker.css" />
     <link rel="shortcut icon" href="/assets/images/Favicon.png" type="image/x-icon">
-    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    @vite('resources/css/styles.css')
 </head>
 
 <body>
